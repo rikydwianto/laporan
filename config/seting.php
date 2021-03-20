1c4e4543730b549aa348b567f54b4c454dda3cd4
Riky DWianto <rikydwianto04@gmail.com>
2021-03-20 10:49:58 +0700

    pmb

--- config/seting.php
+++ config/seting.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$username = 'komw2213';
-$password = '8dzX23xCGDxi41';
-$db_name = 'komw2213_komida'; // nama databasenya
+$username = 'root';
+$password = 'root';
+$db_name = 'komida1'; // nama databasenya
 $server = $_SERVER['HTTP_HOST'];
 $index =  $_SERVER['REQUEST_URI	'];
 $port = ":8080/";
@@ -13,5 +13,5 @@ $menu ="index.php?menu=";
  date_default_timezone_set("Asia/Bangkok");
  error_reporting(0);
  $no=1;
- $url = "https://laporan.komidapagaden.my.id/";
+ $url = "http://localhost:8080/laporan/";
 ?>	
