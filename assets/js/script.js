e3b72cec05af2784084b0dadf7dd26637afe439c
Riky DWianto <rikydwianto04@gmail.com>
2021-03-21 22:13:20 +0700

    menambahkan leaflet

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,8 +65,85 @@ function ganti_bayar(id)
  });
 
 
-
 // $(document).on("change","#wilayah",function(){
 // 	var id = $(this).val();
 // 	alert(id)
 // });
+
+
+var map = L.map('map').setView([-6.449471595334012,107.81619415504505], 14);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-6.449471595334012,107.81619415504505]).addTo(map)
+    .bindPopup('KOMIDA PAGADEN')
+    .openPopup();
+
+    // ini untuk center aja
+    var text ='';
+    $.getJSON( url_link + "api/peta.php", function( data ) {
+		  var items = [];
+		  $.each( data, function( i, field ) {
+		  	text = "<h2>"+data[i]['nama_lokasi']+"</h2><br> Kategori "+ data[i]['kategori']+" <br/>Center : " + data[i]['center']+" <br/> Keterangan : " + data[i]['keterangan']+" <br/> Alamat : " + data[i]['alamat'];
+		   	L.marker([data[i]['latitude'],data[i]['longitude']]).addTo(map)
+		    .bindPopup(text)
+		    .openPopup();
+
+		  });
+		 
+		  
+		});
+
+function onMapClick(e) {
+  marker = new L.marker(e.latlng, {draggable:'true'});
+
+  marker.on('dragend', function(event){
+    var marker = event.target;
+    var position = marker.getLatLng();
+
+    marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+    map.panTo(new L.LatLng(position.lat, position.lng))
+  });
+  map.addLayer(marker);
+  
+};
+
+map.on('click', onMapClick);
+
+function ambillokasi(){
+         var isi_lat = $("#lat").val();
+        var lat = isi_lat;
+        var lat1 = lat.split(",");
+        var long = lat1[0].split("LngLat(");
+
+        lat1 = lat1[1].split(")");
+        // $("#latitude").val(lat1[0])
+        var latitude = lat1[0].trim();
+        var longitude = long[1].trim();
+        $("#latitude").val(latitude);
+        $("#longitude").val(longitude);
+        $("#getLokasi").attr('href',"https://www.google.com/maps/place/"+latitude+","+longitude+"/"+latitude+","+longitude+",17z/data=!3m1!4b1");  
+    }
+
+
+map.on('click',function(e){
+	$("#lat").val(e.latlng);
+	
+	ambillokasi();
+	var center = $("#link #center").data('link');
+    var anggota = $("#link #anggota").data('link');
+    var pu = $("#link #pu").data('link');
+    alert(pu);	
+    var lat = $("#latitude").val();
+    var lng = $("#longitude").val();
+
+    $("#center").attr('href',center +"&lat="+lat+"&lng="+lng);
+    $("#anggota").attr('href',anggota +"&lat="+lat+"&lng="+lng);
+    $("#pu").attr('href',pu +"&lat="+lat+"&lng="+lng);
+})
+
+
+  
+// map.on('click', onMapClick);
--- input-lokasi.php
+++ input-lokasi.php
@@ -34,11 +34,12 @@ $nama_jabatan=$d['singkatan_jabatan'];
 
     <!-- Custom CSS -->
     <link href="<?=$url ?>assets/css/startmin.css" rel="stylesheet">
-    <link href="<?=$url ?>assets/js/leaflet/leaflet.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
     <link href="<?=$url ?>assets/css/morris.css" rel="stylesheet">
     <link href="<?=$url ?>assets/style.css" rel="stylesheet">
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js'></script>
     <!-- Custom Fonts -->
     <link href="<?=$url ?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.css"/>
@@ -96,25 +97,64 @@ $nama_jabatan=$d['singkatan_jabatan'];
 
 
                     <div id='map' style='width: 100%; height: 500px;'></div>
-                    <script type="text/javascript">
-                       
+                    
+                    <script> mapboxgl.accessToken = 'pk.eyJ1IjoicmlreWR3aWFudG8iLCJhIjoiY2ttaXR2M3MwMGtsODJxbXVpaHZraWI0MiJ9.M-fgWx9RsxU5lFoWiDRRNA';
+                        var map = new mapboxgl.Map({
+                        container: 'map', // container ID
+                        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                        center: [ 107.789884 , -6.510135], // starting position [lng, lat]
+                        zoom: 15 // starting zoom
+                        });
+                        map.addControl(
+                        new mapboxgl.GeolocateControl({
+                        positionOptions: {
+                        enableHighAccuracy: true
+                        },
+                        trackUserLocation: true
+                        })
+                        );
+                        map.addControl(
+                            new mapboxgl.NavigationControl
+                            );
 
-                    </script>
+
+
+
+                        map.on('click', function (e) {
+
+                           
+
+                            $("#lat").val(e.lngLat);
+                        // document.getElementById('info').innerHTML =
+                        // // e.point is the x, y coordinates of the mousemove event relative
+                        // // to the top-left corner of the map
+                        // JSON.stringify(e.point) +
+                        // '<br />' +
+                        // // e.lngLat is the longitude, latitude geographical position of the event
+                        // JSON.stringify(e.lngLat.wrap());
+                        });
+
+
+
+                       
+                        
+            </script>
+
                 </div>
 
                 <div class="col-md-4">
-                    <a href="<?=$url."input-lokasi.php"?>" class="btn btn-danger">Tambah Lokasi</a>
+                     <a href="<?=$url."lokasi.php"?>" class="btn btn-danger">LIHAT Lokasi</a>
                     <input type="hidden" name="coba" id="coba" class="form-control">
                     <input type="hidden" name="lat" id="lat" class="form-control">
                     <input type="hidden"  id="latitude" name="latitude" class='form-control'>
                     <input type="hidden" id="longitude" name="longitude" class='form-control'>
-                    <!-- <ul class="list-group" id="link">
+                    <ul class="list-group" id="link">
                       <li class="list-group-item " >MASUKAN LOKASI</li>
                       <li class="list-group-item "  ><a id="center" data-link="<?=$url.$menu ?>lokasi&pilih=center" href="<?=$url.$menu ?>lokasi&pilih=center" >CENTER</a></li>
                       <li class="list-group-item "><a id="anggota" data-link="<?=$url.$menu ?>lokasi&pilih=anggota"  href="<?=$url.$menu ?>lokasi&pilih=anggota">ANGOTA</a></li>
                       <li class="list-group-item "><a data-link="<?=$url.$menu ?>lokasi&pilih=pu" id="pu"  href="<?=$url.$menu ?>lokasi&pilih=pu">PU</a></li>
                       <li class="list-group-item "><a id='getLokasi'>GET DIRECTION</a></li>
-                    </ul> -->
+                    </ul>
                 </div>
                 <?php
 			}
@@ -135,18 +175,56 @@ $nama_jabatan=$d['singkatan_jabatan'];
 
 <!-- Custom Theme JavaScript -->
 <script src="<?=$url ?>assets/js/startmin.js"></script>
-<script src="<?=$url ?>assets/js/leaflet/leaflet.js"></script>
 <script src="<?=$url ?>assets/js/popper.min.js"></script>
 <script src="<?=$url ?>assets/js/morris.min.js"></script>
 <!-- <script src="<?=$url ?>assets/js/morris.data.js"></script> -->
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
-<script type="text/javascript">
-    var url_link = "<?=$url ?>";
-    
-
-</script>
 
 
 <script src="<?=$url ?>assets/js/script.js"></script>
+<script type="text/javascript">
+    
+    function ambillokasi(){
+         var isi_lat = $("#lat").val();
+        var lat = isi_lat;
+        var lat1 = lat.split(",");
+        var long = lat1[0].split("LngLat(");
+
+        lat1 = lat1[1].split(")");
+        // $("#latitude").val(lat1[0])
+        var latitude = lat1[0].trim();
+        var longitude = long[1].trim();
+        $("#latitude").val(latitude);
+        $("#longitude").val(longitude);
+        $("#getLokasi").attr('href',"https://www.google.com/maps/place/"+latitude+","+longitude+"/"+latitude+","+longitude+",17z/data=!3m1!4b1");  
+
+
+    
+
+    }
+
+
+
+    $("#map").on('click',function(){
+        ambillokasi();
+
+    var center = $("#link #center").data('link');
+    var anggota = $("#link #anggota").data('link');
+    var pu = $("#link #pu").data('link');
+    var lat = $("#latitude").val();
+    var lng = $("#longitude").val();
+
+
+    $("#center").attr('href',center +"&lat="+lat+"&lng="+lng);
+    $("#anggota").attr('href',anggota +"&lat="+lat+"&lng="+lng);
+    $("#pu").attr('href',pu +"&lat="+lat+"&lng="+lng);
+    });
+
+
+
+
+    
+   
+</script>
 </body>
 </html>
