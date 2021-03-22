83b77d1ad733af302237c690490400911cee2380
rikydwianto <80996401+rikydwianto@users.noreply.github.com>
2021-03-22 23:27:18 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,7 +112,7 @@ function onMapClick(e) {
     var position = marker.getLatLng();
 
     marker.setLatLng(L.LatLng(position.lat, position.lng),{draggable:'false'});
-    map.panTo( L.LatLng(position.lat, position.lng))
+    map.panTo( L.LatLng(position.lat, position.lng)).bindPopup(');
   });
   map.addLayer(marker);
 
@@ -135,20 +135,4 @@ map.on('click', function(e) {
     $("#pu").attr('href',pu +"&lat="+lat+"&lng="+lng);
 });
 
-
-L.control.coordinates({
-	position:"bottomleft", //optional default "bootomright"
-	decimals:2, //optional default 4
-	decimalSeperator:".", //optional default "."
-	labelTemplateLat:"Latitude: {y}", //optional default "Lat: {y}"
-	labelTemplateLng:"Longitude: {x}", //optional default "Lng: {x}"
-	enableUserInput:true, //optional default true
-	useDMS:false, //optional default false
-	useLatLngOrder: true, //ordering of labels, default false-> lng-lat
-	markerType: L.marker, //optional default L.marker
-	markerProps: {}, //optional default {},
-	labelFormatterLng : function(lng){return lng+" lng"}, //optional default none,
-	labelFormatterLat : function(lat){return lat+" lat"}, //optional default none
-	customLabelFcn: function(latLonObj, opts) { "Geohash: " + encodeGeoHash(latLonObj.lat, latLonObj.lng)} //optional default none
-}).addTo(map);
 
