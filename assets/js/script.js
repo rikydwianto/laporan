384bbbb7d6c633e08ae62ad9b49c6e879a477110
rikydwianto <80996401+rikydwianto@users.noreply.github.com>
2021-03-26 07:10:32 +0700

    Tambahlokasi sekarang

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+
 //setting ICON
 var kantor = L.icon({
     iconUrl: url_link+'assets/img/icon/kantor.png',
