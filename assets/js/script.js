e1e6eccb9880252796f615d214b01497fedf9775
rikydwianto <80996401+rikydwianto@users.noreply.github.com>
2021-03-26 08:28:39 +0700

    Tambah akurasi

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,8 +94,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function onLocationFound(e) {
     var radius = e.accuracy;
 
+//Tambah lokasi
+
+$("#latitude").val(e.latlng.lat);
+    $("#longitude").val(e.latlng.lng);
+    var center = $("#link #center").data('link');
+    var anggota = $("#link #anggota").data('link');
+    var pu = $("#link #pu").data('link');
+    var lat = $("#latitude").val();
+    var lng = $("#longitude").val();
+
+
+var text = "<h3>Tambah Lokasi</h3> ";
+text += "<br><a class='btn' href='"+ url_link+"index.php?menu=lokasi&pilih=center" +"&lat="+e.latlng.lat+"&lng="+e.latlng.lng+"'>Center</a>";
+text += "<br><a class='btn' href='"+ url_link+"index.php?menu=lokasi&pilih=anggota" +"&lat="+e.latlng.lat+"&lng="+e.latlng.lng+"'>ANGGOTA</a>";
+text += "<br><a class='btn' href='"+ url_link+"index.php?menu=lokasi&pilih=pu" +"&lat="+e.latlng.lat+"&lng="+e.latlng.lng+"'>INFORMASI LAINYA</a>";
+
+
+
+
+
     L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        .bindPopup(text).openPopup();
  map = L.map('map').setView([e.latlng.lat,e.latlng.lng], 14);
     L.circle(e.latlng, radius).addTo(map);
 }
