65e33830fe8eefbfe3787f826a4128032339299d
rikydwianto <rikydwianto04@gmail.com>
2021-06-06 09:49:31 +0700

    mengubah koneksi agar lokal/server1

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,13 +1,6 @@
 <?php
 
-
+$con = mysqli_connect($host, $username, $password, $db_name);
 session_start();
-if(file_exists("config/seting.php")){
-    $con = mysqli_connect($host, $username, $password, $db_name);
-}
-else{
-    
-    include("config/server.php");
-    $con = mysqli_connect($host, $username, $password, $db_name);
-}
+
 ?> 
