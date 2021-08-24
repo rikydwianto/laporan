31b4f2f238d96080e03c39ca106920acec620e45
rikydwianto <rikydwianto04@gmail.com>
2021-08-24 17:54:41 +0700

    fix penarikan

--- proses/list_penarikan.php
+++ proses/list_penarikan.php
@@ -1,13 +1,27 @@
 
+<?php
+	 if(isset($_GET['tgl']))
+	{
+		$qtgl=$_GET['tgl'];
+	}
+	else{
+		$qtgl=date("Y-m-d");
+	}
+?>
 <div class='content table-responsive'>
     <h2 class='page-header'>Penarikan Simpanan oleh Manager</h2>
     <a href="<?=$url.$menu?>penarikan_simpanan&list=cari&tglawal=<?=$tglawal?>&tglakhir=<?=$tglakhir?>&cari=FILTER" class='btn btn-success'>
         <i class="fa fa-plus"></i> Tambah
     </a>
-    <a href="<?=$url."export/penarikan_simpanan.php?"?>upk&list=cari&tglawal=<?=$tglawal?>&tglakhir=<?=$tglakhir?>&cari=FILTER" class='btn btn-danger'>
+    <a href="<?=$url."export/penarikan_simpanan.php?"?>upk&tgl=<?=$qtgl?>" class='btn btn-danger'>
         <i class="fa fa-file-excel-o"></i> EXPORT
     </a>
     <br>
+    <form method='get' action='<?php echo $url.$menu ?>list_penarikan'>
+		<input type=hidden name='menu' value='list_penarikan' />
+		<input type=date name='tgl' value='<?php echo isset($_GET['tgl']) ? $_GET['tgl'] : date("Y-m-d") ?>' onchange="submit()" />
+		<input type=submit name='cari' value='CARI' />
+		</form>
     <table class='table table-bordered'>
         <tr>
             <th>No</th>
@@ -23,7 +37,7 @@
         <?php 
         $tgl =date("Y-m-d");
         $total_penarikan=0;
-        $penarikan = mysqli_query($con,"SELECT * FROM penarikan_simpanan left JOIN daftar_nasabah ON daftar_nasabah.`id_nasabah`=penarikan_simpanan.`id_anggota` join karyawan on karyawan.id_karyawan=penarikan_simpanan.id_karyawan where penarikan_simpanan.tgl_penarikan='$tgl' and penarikan_simpanan.id_cabang='$id_cabang' order by karyawan.nama_karyawan asc");
+        $penarikan = mysqli_query($con,"SELECT * FROM penarikan_simpanan left JOIN daftar_nasabah ON daftar_nasabah.`id_nasabah`=penarikan_simpanan.`id_anggota` join karyawan on karyawan.id_karyawan=penarikan_simpanan.id_karyawan where penarikan_simpanan.tgl_penarikan='$qtgl' and penarikan_simpanan.id_cabang='$id_cabang'  order by karyawan.nama_karyawan asc");
         while($simp = mysqli_fetch_array($penarikan))
         {
             $total_penarikan = $total_penarikan + $simp['nominal_penarikan'];
