191ad8a3ed5a23016310aa4ead5ab2ee64afcb16
Riky Dwianto <rikydwianto04@gmail.com>
2021-10-04 22:09:05 +0700

    monitoring dabe

--- api/monitoring.php
+++ api/monitoring.php
@@ -10,8 +10,18 @@ if (isset($_GET['mtr']) && isset($_GET['mtr'])) {
     $id  = aman($con,$_GET['id']);
     $mtr  = $_GET['mtr'];
     $detail  = $_GET['detail'];
+
     $edit  = mysqli_query($con, "update pinjaman set monitoring='$mtr' where id_pinjaman='$id'");
     $keluan  = mysqli_query($con, "update banding_monitoring set status='selesai' where id_detail_pinjaman='$detail'");
+    if($mtr=='sudah'){
+        $input = mysqli_query($con,"INSERT INTO `monitoring` (`id_pinjaman`,`id_detail_pinjaman`, `tgl_monitoring`) VALUES ('$id','$detail', curdate()); ");   
+        
+    }
+    else{
+        $input = mysqli_query($con,"DELETE FROM `monitoring` WHERE `id_pinjaman` = '$id'; ");
+    }
+
+
     if ($edit) {
         echo "berhasil";
     } else {
--- proses/monitoring.php
+++ proses/monitoring.php
@@ -397,7 +397,97 @@
         </form>
     <?php
     } 
-   
+   else if(isset($_GET['riwayat'])){
+    //RIWAYAT MONITORING
+
+    ?>
+    <h2>RIWAYAT MONITORING</h2>
+    <TABLE class='table' id='data_karyawan'>
+        <thead>
+            <tr>
+                <!-- <th>no</th> -->
+                <th>Staff</th>
+                <th>NO Pinjaman</th>
+                <th>Nama</th>
+                <th>Jumlah Pinjaman</th>
+                <th>Produk</th>
+                <th>Cair</th>
+
+                <th>TGL MTR</th>
+                <th>#</th>
+
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php $q = mysqli_query($con, "select *,DATEDIFF(CURDATE(), tgl_cair) as total_hari from monitoring 
+            left join pinjaman on pinjaman.id_pinjaman=monitoring.id_pinjaman
+            left join karyawan on karyawan.id_karyawan=pinjaman.id_karyawan 
+                            
+                            where pinjaman.id_cabang='$id_cabang' order by karyawan.nama_karyawan asc");
+            while ($pinj = mysqli_fetch_array($q)) {
+                if ($pinj['total_hari'] > 14) {
+                    $tr = "#ffd4d4";
+                } else $tr = "#fffff";
+            ?>
+                <tr style="background:<?= $tr ?>">
+                    <!-- <td><?= $no++ ?></td> -->
+                    <td><?= $pinj['nama_karyawan'] ?></td>
+                    <td><?= ganti_karakter($pinj['id_detail_pinjaman']) ?></td>
+                    <td>
+                        <?= $pinj['nama_nasabah'] ?>
+
+
+                    </td>
+                    <td><?= $pinj['jumlah_pinjaman'] ?></td>
+                    <td>
+
+
+                        <?php
+
+                        $produk = strtolower($pinj['produk']);
+                        if ($produk == "pinjaman umum") $kode = "P.U";
+                        else if ($produk == "pinjaman sanitasi") $kode = "PSA";
+                        else if ($produk == "pinjaman mikrobisnis") $kode = "PMB";
+                        else if ($produk == "pinjaman arta") $kode = "ARTA";
+                        else if ($produk == "pinjaman dt. pendidikan") $kode = "PPD";
+                        else if ($produk == "pinjaman renovasirumah") $kode = "PRR";
+                        else $kode = "LL";
+
+                        echo $kode; ?>(<?= $pinj['pinjaman_ke'] ?>)
+                    </td>
+                    <td><?= $pinj['tgl_cair'] ?></td>
+
+                    <td>
+                        <?php
+                        if ($pinj['monitoring'] == 'belum') {
+                            $tombol = "btn-danger";
+                            $icon = "";
+                        } elseif ($pinj['monitoring'] == 'sudah') {
+
+                            $tombol = "btn-info";
+                            $icon = $pinj['tgl_monitoring'];
+                        } else $tombol = "btn-danger";
+                        ?>
+                        <span class="pull-right" id='loading_<?= $pinj['id_pinjaman'] ?>' class="badge rounded-pill bg-danger"></span>
+                        <?= $icon ?>
+                        
+                    </td>
+                    <td>
+                    <a href="#modalku1" id="custId" class="btn btn-warning" data-toggle="modal" data-id="<?= $pinj['id_pinjaman'] ?>">Detail</a>
+                    <input type="button" id="cek_<?= $pinj['id_pinjaman'] ?>" class='btn <?= $tombol ?>' value='<?= $pinj['monitoring'] ?>' onclick="monitoring('<?= $pinj['id_pinjaman'] ?>','<?= $pinj['id_detail_pinjaman'] ?>')" id="">
+
+                    </td>
+                </tr>
+
+            <?php
+            }
+            ?>
+        </tbody>
+        </form>
+    </TABLE>
+    <?php
+   }
     else {
     
     
@@ -411,10 +501,12 @@
             <!-- <input type="submit" value="SIMPAN" name='mtr' class='btn btn-danger'> -->
             <a href="<?= $url . $menu ?>monitoring" class='btn btn-success'> <i class="fa fa-list-ol"></i> Lihat yang belum</a>
             <a href="<?= $url . $menu ?>monitoring&filter" class='btn btn-info'> <i class="fa fa-book"></i> Lihat Semua Data</a>
-            <a href="<?= $url . $menu ?>monitoring&banding" class='btn btn-warning'> <i class="fa fa-bell"></i> KELUHAN(<?= $hitung_banding ?>)</a> <br /><br />
+            <a href="<?= $url . $menu ?>monitoring&banding" class='btn btn-warning'> <i class="fa fa-bell"></i> KELUHAN(<?= $hitung_banding ?>)</a> 
+            <a href="<?= $url . $menu ?>monitoring&riwayat" class='btn btn-info'> <i class="fa fa-check"></i> Riwayat Monitoring</a> <br /><br />
             <a href="<?= $url . $menu ?>monitoring&tgl=14" class='btn btn-danger'> <i class="fa fa-angle-right"></i> Lebih 14 hari</a> 
             <a href="<?= $url . $menu ?>monitoring&tgl=21" class='btn btn-danger'> <i class="fa fa-angle-right"></i> Lebih 21 hari</a> 
             <a href="<?= $url . $menu ?>monitoring&tgl=30" class='btn btn-danger'> <i class="fa fa-angle-right"></i> Lebih 30 hari</a> <br /><br />
+            
             <TABLE class='table' id='data_karyawan'>
                 <thead>
                     <tr>
