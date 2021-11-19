80daf3300d0bcaaedc1a81f49ebd3b6fb0a3934f
Riky Dwianto <rikydwianto04@gmail.com>
2021-11-19 18:10:51 +0700

    add blk

--- export/coba.php
+++ export/coba.php
@@ -10,37 +10,31 @@ $cabang= $_SESSION['cabang'];
 $id_cabang= $_SESSION['cabang'];
 $su= $_SESSION['su'];
 require("../vendor/PHPExcel/Classes/PHPExcel.php");
-$path = "../RAHASIA/nasabah_keluar.xlsx";
+$path = "../RAHASIA/blk.xlsx";
 $reader = PHPExcel_IOFactory::createReaderForFile($path);
 $objek = $reader->load($path);
 $ws = $objek->getActiveSheet();
 $last_row = $ws->getHighestDataRow();
 
-for($row = 4;$row<=$last_row;$row++){
-    $no_id =  $ws->getCell("D" . $row)->getValue();
-    if($no_id==null){
+for($row = 7;$row<=$last_row;$row++){
+    $kode_pemb =  $ws->getCell("C" . $row)->getValue();
+    if($kode_pemb==null){
         
     }
     else{
-        $agt = (substr($no_id,0,3));
+        $agt = (substr($kode_pemb,0,3));
         // echo $agt;
-        if($agt=='AGT'){
-            $id_nasabah =  $ws->getCell("D" . $row)->getValue();
-            $ID = explode("-",$id_nasabah)[1];
+        if($kode_pemb==' PU ' || $kode_pemb==' PMB ' || $kode_pemb==' PSA ' || $kode_pemb==' PRR ' || $kode_pemb==' PPD '  ){
+            $id_nasabah =  $ws->getCell("A" . $row)->getValue();
             
-            $nasabah =  ($ws->getCell("E".$row)->getValue());
-            $alasan = ($ws->getCell("J".$row)->getValue());
-            $staff = ($ws->getCell("A".$row)->getValue());
-            $ID = sprintf("%0d",$ID);
-            $center = $ws->getCell("C".$row)->getValue();
-            $tgl = $ws->getCell("I".$row)->getValue();
-            $tgl =  date("Y-m-d", PHPExcel_Shared_Date::ExcelToPHP($tgl));
-            $center = str_replace(" ","",explode("/",$center)[0]);
-            $cari_staff  = mysqli_query($con,"select * from center join karyawan on karyawan.id_karyawan=center.id_karyawan where no_center='$center'");
-            $cari_staff = mysqli_fetch_array($cari_staff);
-            // echo $no++.$cari_staff['nama_karyawan']."<br/>";
-            // echo $no++.$id_nasabah.'  -  '.$nasabah." - ".$alasan."<br/>";
-            // mysqli_query($con,"INSERT INTO `temp_anggota` (`staff`, `tgl_bergabung`, `status_input`, `id_cabang`) VALUES ('$staff', '$gabung', 'belum', '$id_cabang'); ");
+            $nasabah =  ($ws->getCell("B".$row)->getValue());
+            // $tgl = $ws->getCell("I".$row)->getValue();
+            // $tgl =  date("Y-m-d", PHPExcel_Shared_Date::ExcelToPHP($tgl));
+            $pokok = (int)ganti_karakter($ws->getCell("I".$row)->getValue());
+            $margin = (int)ganti_karakter($ws->getCell("J".$row)->getValue());
+            $pensiun = (int)ganti_karakter($ws->getCell("S".$row)->getValue());
+            $sukarela = (int)ganti_karakter($ws->getCell("p".$row)->getValue());
+            
         }
     }
 }
