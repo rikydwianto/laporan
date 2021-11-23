d65f7000c82ec3e7b02e5ceb5d0045d4111abe40
Riky Dwianto <rikydwianto04@gmail.com>
2021-11-23 23:57:00 +0700

    lower

--- proses/daftar_nasabah.php
+++ proses/daftar_nasabah.php
@@ -167,7 +167,7 @@
             while($cek_center =mysqli_fetch_array($qcek_center)){
                 $hitung_center = mysqli_fetch_array(mysqli_query($con,"select * from center where no_center='$cek_center[no_center]' and id_cabang='$id_cabang'"));
                 if($hitung_center){
-                    $hari = ($cek_center['hari']);
+                    $hari = strtolower($cek_center['hari']);
                     mysqli_query($con,"UPDATE `center` SET `member_center` = '$cek_center[total_anggota]',hari='$hari' WHERE `no_center` = '$cek_center[no_center]' and id_cabang='$id_cabang'; ");
                     // echo $cek_center['no_center'].' - '.$cek_center['id_karyawan']."|".$cek_center['total_anggota']."<br/>";
                 }
