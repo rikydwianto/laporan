7431efa2b5567243fc2926470e5890b6fe981a67
Riky Dwianto <rikydwianto04@gmail.com>
2021-12-21 08:21:17 +0700

    print angttoa tidak bayar

--- proses/anal_bayar.php
+++ proses/anal_bayar.php
@@ -52,74 +52,94 @@
     <?php 
     if(isset($_GET['cek_anal_bayar'])){
         ?>
-        <table class='table'>
-            <tr>
-                <th>NO</th>
-                <th>CENTER</th>
-                <th>ID</th>
-                <th>NASABAH</th>
-                <th>OS <br/><?=$tgl_awal?></th>
-                <th>OS <br/><?=$tgl_akhir?></th>
-                <th>TOTAL SAVING <br/><?=$tgl_awal?></th>
-                <th>TOTAL SAVING <br/><?=$tgl_akhir?></th>
-                <th>SELISIH</th>
-                <th>PAR?</th>
-                <th>STAFF</th>
-            </tr>
-            <?php 
-            $cek_q1 = mysqli_query($con,"select * from blk b join karyawan k on k.id_karyawan=b.id_karyawan where b.id_cabang='$id_cabang' and tanggal_blk='$tgl_awal' order by k.nama_karyawan,b.center asc");
-            while($row1 = mysqli_fetch_array($cek_q1)){
-                $cek_q2 = mysqli_query($con,"select * from blk b join karyawan k on k.id_karyawan=b.id_karyawan where b.id_anggota='$row1[id_anggota]' and b.id_cabang='$id_cabang' and tanggal_blk='$tgl_akhir' order by k.nama_karyawan asc");
-                $row2 = mysqli_fetch_array($cek_q2);
-                $os1 = $row1['outstanding'];
-                $os2 = $row2['outstanding'];
-                $sukarela1 = $row1['sukarela'];
-                $wajib1 = $row1['wajib'];
-                $pensiun1 = $row1['pensiun'];
-                $sukarela2 = $row2['sukarela'];
-                $wajib2 = $row2['wajib'];
-                $pensiun2 = $row2['pensiun'];
+        <a onclick="printPageArea('tidak_bayar')">Print</a>
+        <div id='tidak_bayar'>
+            <table class='table' >
+                <tr>
+                    <th>NO</th>
+                    <th>CENTER</th>
+                    <th>ID</th>
+                    <th>NASABAH</th>
+                    <th>OS <br/><?=$tgl_awal?></th>
+                    <th>OS <br/><?=$tgl_akhir?></th>
+                    <th>TOTAL SAVING <br/><?=$tgl_awal?></th>
+                    <th>TOTAL SAVING <br/><?=$tgl_akhir?></th>
+                    <th>SELISIH</th>
+                    <th>PAR?</th>
+                    <th>STAFF</th>
+                </tr>
+                <?php 
+                $cek_q1 = mysqli_query($con,"select * from blk b join karyawan k on k.id_karyawan=b.id_karyawan where b.id_cabang='$id_cabang' and tanggal_blk='$tgl_awal' order by k.nama_karyawan,b.center asc");
+                while($row1 = mysqli_fetch_array($cek_q1)){
+                    $cek_q2 = mysqli_query($con,"select * from blk b join karyawan k on k.id_karyawan=b.id_karyawan where b.id_anggota='$row1[id_anggota]' and b.id_cabang='$id_cabang' and tanggal_blk='$tgl_akhir' order by k.nama_karyawan asc");
+                    $row2 = mysqli_fetch_array($cek_q2);
+                    $os1 = $row1['outstanding'];
+                    $os2 = $row2['outstanding'];
+                    $sukarela1 = $row1['sukarela'];
+                    $wajib1 = $row1['wajib'];
+                    $pensiun1 = $row1['pensiun'];
+                    $sukarela2 = $row2['sukarela'];
+                    $wajib2 = $row2['wajib'];
+                    $pensiun2 = $row2['pensiun'];
 
-                $simpanan1 = $sukarela1 + $pensiun1 + $wajib1;
-                $simpanan2 = $sukarela2 + $pensiun2 + $wajib2;
-                $selisih = $sukarela2 - $sukarela1;
-                if($os1==$os2 && ($simpanan1 == $simpanan2 || $selisih>-5000 && $selisih<10000)){
-                    mysqli_query($con,"UPDATE blk set sinkron_sl='sudah' where id='$row1[id]' and id_cabang='$id_cabang'");
-                    echo mysqli_error($con);
-                    // echo "UPDATE blk set sinkron_sl='sudah' where id='$row1[id]' and id_cabang='$id_cabang'";
+                    $simpanan1 = $sukarela1 + $pensiun1 + $wajib1;
+                    $simpanan2 = $sukarela2 + $pensiun2 + $wajib2;
+                    $selisih = $sukarela2 - $sukarela1;
+                    if($os1==$os2 && ($simpanan1 == $simpanan2 || $selisih>-5000 && $selisih<10000)){
+                        mysqli_query($con,"UPDATE blk set sinkron_sl='sudah' where id='$row1[id]' and id_cabang='$id_cabang'");
+                        echo mysqli_error($con);
+                        // echo "UPDATE blk set sinkron_sl='sudah' where id='$row1[id]' and id_cabang='$id_cabang'";
+                    }
+
+
+
+                    if($os1==$os2 && ($simpanan1 == $simpanan2 || $selisih>-5000 && $selisih<10000) ){
+                        // && ($sukarela1 == $sukarela2 || $selisih>5000 && $selisih<10000)
+                        ?>
+                        <tr>
+                            <td><?=$no++?></td>
+                            <td><?=$row1['center']?></td>
+                            <td><?=$row1['id_anggota']?></td>
+                            <td><?=$row1['nama_nasabah']?></td>
+                            <td><?=angka($os1)?></td>
+                            <td><?=angka($os2)?></td>
+                            <td><?=angka($simpanan1)?></td>
+                            <td><?=angka($simpanan2)?></td>
+                            <td><?=angka($selisih)?></td>
+                            <td><?=($row1['par'])?></td>
+                            <td><?=$row1['nama_karyawan']?></td>
+                        </tr>
+
+                        <?php 
+                    }
+                    else{
+                        mysqli_query($con,"delete from blk where id='$row1[id]'");
+                    }
                 }
+                ?>
 
-
-
-                if($os1==$os2 && ($simpanan1 == $simpanan2 || $selisih>-5000 && $selisih<10000) ){
-                    // && ($sukarela1 == $sukarela2 || $selisih>5000 && $selisih<10000)
-                    ?>
-                    <tr>
-                        <td><?=$no++?></td>
-                        <td><?=$row1['center']?></td>
-                        <td><?=$row1['id_anggota']?></td>
-                        <td><?=$row1['nama_nasabah']?></td>
-                        <td><?=angka($os1)?></td>
-                        <td><?=angka($os2)?></td>
-                        <td><?=angka($simpanan1)?></td>
-                        <td><?=angka($simpanan2)?></td>
-                        <td><?=angka($selisih)?></td>
-                        <td><?=($row1['par'])?></td>
-                        <td><?=$row1['nama_karyawan']?></td>
-                    </tr>
-
-                    <?php 
-                }
-                else{
-                    mysqli_query($con,"delete from blk where id='$row1[id]'");
-                }
-            }
-            ?>
-
-        </table>
+            </table>
+        </div>
         <?php
     }
     ?>
 
 
 </div>
+<script>
+    function printPageArea(areaID){
+    var printContent = document.getElementById(areaID);
+    var WinPrint = window.open('', '', 'width=900,height=650');
+    WinPrint.document.write(printContent.innerHTML);
+    WinPrint.document.close();
+    WinPrint.focus();
+    WinPrint.print();
+    WinPrint.close();
+}
+</script>
+<style>
+    table { page-break-inside:auto }
+    tr    { page-break-inside:avoid; page-break-after:auto }
+    thead { display:table-header-group }
+    tfoot { display:table-footer-group }
+</style>
