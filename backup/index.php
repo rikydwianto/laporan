e81db1af6b09b1949f54ad4022541672174c2593
Riky Dwianto <rikydwianto04@gmail.com>
2022-01-04 06:33:16 +0700

    Backup

--- backup/index.php
+++ backup/index.php
@@ -112,7 +112,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'rikydwianto08@gmail.com';                     //SMTP username
+    $mail->Username   = 'rikydwianto04@gmail.com';                     //SMTP username
     $mail->Password   = 'bandos55';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
