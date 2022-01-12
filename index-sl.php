b3c3593c7832170f837d8b6a26203cbc28daa818
Riky Dwianto <rikydwianto04@gmail.com>
2022-01-13 06:53:41 +0700

    link penurunan par

--- index-sl.php
+++ index-sl.php
@@ -103,7 +103,7 @@ $cek_upk = mysqli_query($con,"select * from upk join center on center.no_center=
         <?php
     }
     ?>
-    <!-- <a href="<?=$url.$menu."ket_laporan&id_laporan=$cek_laporan[id_laporan]&id=$cek_laporan[id_karyawan]"?>" class="btn">LINK PENURUNAN PAR</a> -->
+    <a href="<?=$url.$menu."ket_laporan&id_laporan=$cek_laporan[id_laporan]&id=$cek_laporan[id_karyawan]"?>" class="btn">LINK PENURUNAN PAR</a>
     <table class='table'>
         <tr>
             <th>No. CTR</th>
