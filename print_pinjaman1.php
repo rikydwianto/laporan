3cd7b185d3932b01485d987e89bce5996355ad2d
Riky Dwianto <rikydwianto04@gmail.com>
2022-02-07 18:03:51 +0700

    daftar pinjaman v2

---
+++ print_pinjaman1.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once "config/seting.php";
+require_once "config/koneksi.php";
+require_once("proses/fungsi.php");
+
+require_once("model/model.php");
+require("vendor/PHPExcel/Classes/PHPExcel.php");
+$id_karyawan = $_SESSION['id'];
+$tgl_awal = $_GET['tgl_awal'];
+$tgl_akhir = $_GET['tgl_akhir'];
+// require 'vendor/autoload.php';
+
+// use Dompdf\Dompdf;
+
+// // instantiate and use the dompdf class
+// $dompdf = new Dompdf();
+// $dompdf->loadHtml('hello world');
+
+// // (Optional) Setup the paper size and orientation
+// $dompdf->setPaper('F4', 'landscape');
+
+// // Render the HTML as PDF
+// $dompdf->render();
+
+// // Output the generated PDF to Browser
+// $dompdf->stream();
+// $url="http://192.168.100.6/laporan/";
+$nama_karyawan = $_SESSION['nama_karyawan'];
+$jabatan = $_SESSION['jabatan'];
+$cabang = $_SESSION['cabang'];
+$id_cabang = $_SESSION['cabang'];
+$su = $_SESSION['su'];
+$d = detail_karyawan($con, $id_karyawan);
+$nama_jabatan = $d['singkatan_jabatan'];
+$_SESSION['kode_cabang']=$d['kode_cabang'];?>
+<head>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+
+        }
+        table tr th{
+            height: 0.7cm;
+            padding:3px;
+            font-weight: bold;
+            font-size: 11px;
+        }
+
+        .kertas table{
+            font-size: 8px;
+            margin-left: 0cm;
+            margin-right:0.2cm;
+        }
+        .kertas{
+            /* background-color: red; */
+            width: 100%;
+            /* height: 21cm; */
+        }
+        .kertas .tengah{
+            padding-top: 0.5cm;
+            text-align: center;
+            line-height: 0cm
+        }
+        table tr td{
+            vertical-align: middle;
+
+        }
+        .isi_tengah{
+            text-align: center;
+        }
+        .kecil{
+            font-size: 10px;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Daftar Pinjaman</title>
+</head>
+<body>
+    <div class='kertas'>
+        <div class='tengah'>
+            <h1>DAFTAR PINJAMAN</h1>
+            <h3>CABANG <?=strtoupper($d['nama_cabang'])?></h3>
+            <h3>Periode : <?=format_hari_tanggal($tgl_awal)?> s/d <?=format_hari_tanggal($tgl_akhir)?></h3>
+
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th rowspan="2" style='font-size: 8px;'>NO</th>
+                    <th colspan="3" style='font-size: 8px;' >NASABAH</th>
+                    <th rowspan="2" style='font-size: 8px;' >PHONE</th>
+                    <th rowspan="2" style='font-size: 8px;' >CENTER</th>
+                    <th rowspan="2" style='font-size: 8px;' >GROUP</th>
+                    <th rowspan="2" style='font-size: 8px;' >PRODUK</th>
+                    <th rowspan="2" style='font-size: 8px;'>JUMLAH.<br/>PINJAMAN</th>
+                    <th rowspan="2" style='font-size: 8px;' >OUTSTANDING</th>
+                    <th rowspan="2" style='font-size: 8px;'>J.<br/>Waktu</th>
+                    <th rowspan="2" style='font-size: 8px;'>RATE (%)</th>
+                    <th rowspan="2" style='font-size: 8px;' >Angsuran</th>
+                    <th rowspan="2" style='font-size: 6px;'>TUJUAN <br/>PINJAMAN</th>
+                    <th rowspan="2" style='font-size: 8px;' >KE</th>
+                    <th rowspan="2" style='font-size: 10px;width:12cm'>NAMA F.O.</th>
+                    <th colspan="7" style='font-size: 8px;'>KETERANGAN</th>
+                </tr>
+                <tr>
+                    <th >ID</th>
+                    <th >ID.PINJAMAN</th>
+                    <th >NAMA LENGKAP</th>
+                    
+                    <th style='font-size: 10px;' >PENCAIRAN </th>
+                    <th style="width:12cm">KETERANGAN</th>
+                    <th style="font-size:8px">PARAF</th>
+                </tr>
+            </thead>
+            <?php 
+            $total_disburse = 0;
+            $q = mysqli_query($con,"SELECT * from pinjaman p
+            join karyawan k on k.id_karyawan=p.id_karyawan
+             where p.id_cabang='$id_cabang' and p.tgl_cair between '$tgl_awal' and '$tgl_akhir'");
+             echo mysqli_error($con);
+             while($r=mysqli_fetch_array($q)){
+                 $total_disburse += $r['jumlah_pinjaman'];
+                 $group = explode("/",$r['id_detail_nasabah'])[2];
+                 ?>
+                 <tr >
+                    <td style="height: 2.5px;" colspan="" ><?=$no++?>.</td>
+                    <td class="kecil"><?=$r['id_detail_nasabah']?></td>
+                    <td class="kecil"><?=$r['id_detail_pinjaman']?></td>
+                    <td ><?=$r['nama_nasabah']?></td>
+                    <td ><?=$r['no_hp']?></td>
+                    <td class='kecil'><?=$r['center']?></td>
+                    <td class='isi_tengah'><?=$group?></td>
+                    <td class='kecil'><?=$r['produk']?></td>
+                    <td ><?=angka($r['jumlah_pinjaman'])?></td>
+                    <td ><?=angka($r['outstanding'])?></td>
+                    <td class='isi_tengah'><?=$r['jk_waktu']?></td>
+                    <td class='isi_tengah'  ><?=$r['margin']?></td>
+                    <td ><?=angka($r['angsuran'])?></td>
+                    <td ><?=$r['tujuan_pinjaman']?></td>
+                    <td class='isi_tengah'><?=$r['pinjaman_ke']?></td>
+                    <td class='kecil'><?=$r['nama_karyawan']?></td>
+                    <td class='isi_tengah'><?=$r['tgl_cair']?></td>
+                    <td >
+                   
+                    </td>
+                    <td ></td>
+                 </tr>
+                 <?php
+             }
+            ?>
+
+            
+                <tr>
+                    <th colspan="8">TOTAL</th>
+                    <th colspan="1"><?=angka($total_disburse)?></th>
+                    <th colspan="1"></th>
+                </tr>
+            
+        </table>
+    </div>
+</body>
+<style>
+    table { page-break-inside:auto }
+    tr    { page-break-inside:avoid; page-break-after:auto }
+    thead { display:table-header-group }
+    tfoot { display:table-footer-group }
+</style>
+</html>
--- proses/daftar_pinjaman.php
+++ proses/daftar_pinjaman.php
@@ -1,21 +1,30 @@
 <?php @$tgl_awal  = $_GET['tgl_awal'];
         @$tgl_akhir = $_GET['tgl_akhir']; ?>
-<form action="" method="get" id='form_pinjaman'>
+<!-- <form action="" method="get" id='form_pinjaman'> -->
     <input type="hidden" name="menu" value="monitoring">
         <div class="col-md-4">
             <h3>DARI</h3>
-            <input type="date" value="<?=($tgl_awal==""?date("Y-m-d"):$tgl_awal)?>" required name="tgl_awal" class='form-control' id="">
+            <input type="date" id="tgl1" value="<?=($tgl_awal==""?date("Y-m-d"):$tgl_awal)?>" required name="tgl_awal" class='form-control' >
             
             
         </div>
         <div class="col-md-4">
             <h3>SAMPAI</h3>
-            <input type="date" required value="<?=($tgl_akhir==""?date("Y-m-d"):$tgl_akhir)?>" name="tgl_akhir" class='form-control' id="">
-            <input type="submit" value="PRINT" name='daftar_pinjaman' class='btn btn-danger btn-md btn-info'>
-
+            <input type="date"  id="tgl2" required value="<?=($tgl_akhir==""?date("Y-m-d"):$tgl_akhir)?>" name="tgl_akhir" class='form-control' >
+            <!-- <input type="submit" value="PRINT" name='daftar_pinjaman' class='btn btn-danger btn-md btn-info'> -->
+            <!-- <a href="<?=$url."print_pinjaman.php?menu=monitoring&tgl_awal=".date("Y-m-d")."&tgl_akhir=".date("Y-m-d")?>" id='btn_print1' class="btn btn-primary">Print Daftar pinjaman</a> -->
+            <button onclick="btn_click()" class='btn btn-danger' id='print_biasa'>Print 1</button>
+            <button onclick="btn_click2()" class='btn btn-primary' id='print_biasa'>Print v.2</button>
         </div>
-    </form>
+    <!-- </form> -->
     <script>
+        var url = "<?=$url?>";
+        $("#tgl1").on("change",function(){
+            // alert(1)
+        })
+        var url_upk = url ;
+                // $("#link_upk").attr("href",url_upk);
+
         var myForm = document.getElementById('form_pinjaman');
         myForm.onsubmit = function() {
             var isi = $('#form_pinjaman').serialize();
@@ -23,4 +32,26 @@
             var w = window.open('<?=$url."print_pinjaman.php?"?>'+isi,'Print Daftar Pinjaman','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1200,height=720,left = 0,top = 0');
             this.target = 'Popup_Window';
         };
+        $("#btn_print1").on("click",function() {
+            var isi = $('#form_pinjaman').serialize();
+            // alert(isi);
+            var w = window.open('<?=$url."print_pinjaman.php?"?>'+isi,'Print Daftar Pinjaman','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1200,height=720,left = 0,top = 0');
+            this.target = 'Popup_Window';
+        });
+        function btn_click(){
+            // alert(00);
+            var tgl1 = $("#tgl1").val();
+            var tgl2 = $("#tgl2").val();
+            // alert(tgl1 + tgl2)
+            var w = window.open('<?=$url?>print_pinjaman.php?menu=monitoring&tgl_awal='+tgl1+'&tgl_akhir=' + tgl2,'Print Daftar Pinjaman','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1200,height=720,left = 0,top = 0');
+
+        }
+        function btn_click2(){
+            // alert(00);
+            var tgl1 = $("#tgl1").val();
+            var tgl2 = $("#tgl2").val();
+            // alert(tgl1 + tgl2)
+            var w = window.open('<?=$url?>print_pinjaman1.php?menu=monitoring&tgl_awal='+tgl1+'&tgl_akhir=' + tgl2,'Print Daftar Pinjaman','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=1200,height=720,left = 0,top = 0');
+
+        }
     </script>
