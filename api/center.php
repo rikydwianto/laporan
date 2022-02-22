87d2c0712e645dabd3f52e947fa24949589e1f45
Riky Dwianto <rikydwianto04@gmail.com>
2022-02-22 09:27:56 +0700

    peta

--- api/center.php
+++ api/center.php
@@ -5,7 +5,7 @@ require_once "../config/seting.php";
 require_once "../config/koneksi.php";
 require_once("../proses/fungsi.php");
 require_once("../model/model.php");
-
+$id_cabang = $_SESSION['cabang'];
  //query tabel produk
  $sql="SELECT no_center,hari,jam_center,status_center,anggota_center,member_center,karyawan.nama_karyawan,
  center.latitude,center.longitude,cabang.nama_cabang
@@ -13,7 +13,7 @@ require_once("../model/model.php");
   JOIN karyawan ON karyawan.id_karyawan=center.id_karyawan 
   JOIN cabang ON cabang.id_cabang = center.id_cabang
   WHERE
-  center.latitude !='' AND center.longitude !='' AND center.latitude !='null' AND center.longitude !='null'";
+  center.latitude !='' AND center.longitude !='' AND center.latitude !='null' AND center.longitude !='null' and center.id_cabang='$id_cabang'";
  // echo $sql;
  $query=mysqli_query($con,$sql);
 
