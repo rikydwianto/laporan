335effe9246c55f549a3ea9971529545d0628d52
Riky Dwianto <rikydwianto04@gmail.com>
2022-03-09 22:01:10 +0700

    tambah minggu

--- proses/par.php
+++ proses/par.php
@@ -451,29 +451,67 @@ elseif(isset($_GET['anal_topup'])){
 else{
     ?>
     <div class="col-md-12">
-            <div class="col-md-8">
+            <div class="col-md-12">
 
             <h3>DATA PAR</h3>
-            <table class='table'>
+            <table class='table table-bordered'>
                 <tr>
                     <th>NO</th>
                     <th>Tanggal</th>
                     <th>Hari</th>
                     <th>TOTAL AGT PAR</th>
+                    <th>CTR PAR</th>
+                    <th>OS PAR</th>
                     <th>#</th>
                 </tr>
                 <?php 
-                $q_tgl1 = mysqli_query($con,"SELECT  tgl_input,count(*) as hitung FROM deliquency where id_cabang='$id_cabang' group by tgl_input order by tgl_input desc");
+                $q_tgl1 = mysqli_query($con,"SELECT  tgl_input,count(*) as hitung, sum(sisa_saldo) as total_par FROM deliquency where id_cabang='$id_cabang' group by tgl_input order by tgl_input desc");
                 while($cari = mysqli_fetch_array($q_tgl1)){
-                    
+                    $total_center  = mysqli_query($con,"SELECT COUNT(DISTINCT no_center) as total_center FROM deliquency WHERE id_cabang = '$id_cabang' and tgl_input='$cari[tgl_input]' #GROUP BY tgl_input ");
+                    $total_center = mysqli_fetch_array($total_center);
                     ?>
                     <tr>
                         <td><?=$no++?></td>
                         <td><?=$cari['tgl_input']?></td>
                         <td><?=format_hari_tanggal($cari['tgl_input'])?></td>
                         <td><?=($cari['hitung'])?></td>
+                        <td><?=($total_center['total_center'])?></td>
+                        <td><?=rupiah($cari['total_par'])?></td>
                         <td>
                             <a href="<?=$url.$menu?>par&delete&tgl=<?=$cari['tgl_input']?>" onclick="return window.confirm('apakah yakin menghapus semua data <?=format_hari_tanggal($cari['tgl_input'])?>')" class="btn btn-danger"> <i class="fa fa-times"></i> </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="7">
+                            <?php 
+                            $qminggu = mysqli_query($con,"SELECT DISTINCT minggu as minggu1 from deliquency where tgl_input='$cari[tgl_input]' and id_cabang='$id_cabang' order by minggu asc");
+                            $qminggu1 = mysqli_query($con,"SELECT DISTINCT minggu as minggu1 from deliquency where tgl_input='$cari[tgl_input]' and id_cabang='$id_cabang' order by minggu asc");
+                            ?>
+                            <table class='table table-bordered'>
+                                <tr>
+                                    <th>MINGGU</th>
+                                    <?php 
+                                    while($rminggu = mysqli_fetch_array($qminggu)){
+                                        ?>
+                                        <th><?=$rminggu['minggu1']?></th>
+                                        <?php
+                                    }
+                                    ?>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>JML</td>
+                                    <?php 
+                                    while($rminggu = mysqli_fetch_array($qminggu1)){
+                                        $total_minggu = mysqli_query($con,"select count(id) as total from deliquency where id_cabang='$id_cabang' and tgl_input='$cari[tgl_input]' and minggu='$rminggu[minggu1]' ");
+                                        $total_minggu = mysqli_fetch_array($total_minggu)['total'];
+                                        ?>
+                                        <td><?=$total_minggu?></td>
+                                        <?php
+                                    }
+                                    ?>
+                                </tr>
+                            </table>
                         </td>
                     </tr>
                     <?php
