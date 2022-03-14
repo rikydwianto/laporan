f96ccc5cc929c538b24ae0995a5cd62e7f293c8d
Riky Dwianto <rikydwianto04@gmail.com>
2022-03-14 18:31:14 +0700

    montioring

--- api/monitoring.php
+++ api/monitoring.php
@@ -15,7 +15,9 @@ if (isset($_GET['mtr']) && isset($_GET['mtr'])) {
     $keluan  = mysqli_query($con, "update banding_monitoring set status='selesai' where id_detail_pinjaman='$detail'");
     if($mtr=='sudah'){
         $input = mysqli_query($con,"INSERT INTO `monitoring` (`id_pinjaman`,`id_detail_pinjaman`, `tgl_monitoring`,waktu) VALUES ('$id','$detail', curdate(),current_time()); ");   
-        
+       $text = "$_SESSION[nama_karyawan] sedang input monitoring";
+        $url_tele = "https://api.telegram.org/$token/sendMessage?parse_mode=html&chat_id=1185334687&text=$text&reply_message_id=214&force_reply=true";
+        file_get_contents($url_tele);
     }
     else{
         $input = mysqli_query($con,"DELETE FROM `monitoring` WHERE `id_pinjaman` = '$id'; ");
