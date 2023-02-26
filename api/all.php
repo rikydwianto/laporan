3a8c11486e19c92884b6bf7faed97f93d0676aa1
rikydwianto <rikydwianto04@gmail.com>
2023-02-26 16:20:16 +0700

    aas

--- api/all.php
+++ api/all.php
@@ -33,7 +33,7 @@ else{
                             and cabang.id_wilayah=wilayah.id_wilayah
                             and karyawan.id_karyawan='$id' ");
                     $data = mysqli_fetch_assoc($query);
-                $url_p="https://lh3.googleusercontent.com/jDa12L7bcZC3cstQA5kK9qBSoNCAhynP8mP6Xm3u4Dt_z6pQhXGCYQygfbrBimgJjkp3gIMymacJiqg4PH-Le5ypbSQchERnxgoNP3sPlP2i02hkdpWX-KTXBb3KfCQmUyBgbHMGhojf1rCIanAhgLPXjiEtGE3PuF9rQ4ToLVTqIYP8KiFcPKp491MwlBLJAJqKAwZqx1DGp1Bn6BOqJMAljVMbEqQjog1hYrHdkrPWFeoJTVvPU4ps16QFEK1IP9bDmMaJ_N3GJGV_40df9jS1YJ7fXcgUIvLikgnalRgSjD9IWfkNZxJVM4aegq57WbvZsG3muw6EaNp9GC3lJtVErvzfI8r9hBaydAn0P13y6uMSR4QwcVgjm1Qwd_vw0rZ_JShNxjuD3JEnAbP0jeGrJbhiV5rZt0PFi302PMRpvfubKljgkTB3fS4mcKdUwqOOPXSFH1c37rVNEbd8cHik-IyQrjVtHw2dU4lK9rsz7pQbE4ciujH76H87__1Xaymyqr9ANGl0jOedVhvttvKjAMMWfo3nkbDcJgHFpVHvNgw6quoVBf5mEoKf-tlTSuF9YP4evO8Hb2CjeCeohGPHiZfYmf-HZAb44XUgOhHrOUVJo-Squ9zrfvg4XZ36A4y-2zc9g1YhvKmcG0yiJTEsktFCqwqCmH-apTmsiwxgSVkMKC8ZFSAZujtYSEBysy3tZPIv--5EATBje68gA_UTBORg3ejD4OSr09IHKpjlGqZAoaHIyiqiKmZtU2A5ht44CBI9bAauqLIXotcI1bcLJus5oYu1G7JilUSclC4D3eklimbWAxDtijvof1_VnUiMXUzhlrgmrx12-7BEK8c89qFkvAuXnk3HlCs3IrIBhJGW1hdZ3OjidOp8M1SmmR68mMptzbwOvXMb6Qm5Yh9zn1NTte5nXzGSE-cg-MBrqTLz2qyBqa8kkw5W6jgznp69gzrm8bEepAMHZ-_Yl3RXhcM6BhANqBlRq3bFXK4IzXAkP9wiM1M8FlhYfp3E3pEF4x8WfIdlV0mFw6GKbxUgdXy2LML3ZOPdbOihUZo093ByhsgUuQ-TOvr5ez3u5RcQ5dLGruVTVzl3O6mS6UNpduTYEDTjlEA=w1366-h616-s-no?authuser=0";
+                $url_p="https://laporan-baru.site/rikydwianto.jpg";
                 $data['photo']="$url_p";
             }
             else if($menu=="cari_nasabah"){
