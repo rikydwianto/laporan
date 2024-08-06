9cca3a3d3ccc5d2bcb72f0b3b361093468365729
rikydwianto <rikydwianto04@gmail.com>
2024-08-06 09:07:22 +0700

    laporan dtc

--- proses/laporan_dtc.php
+++ proses/laporan_dtc.php
@@ -5,21 +5,21 @@
         <form action="" method="get">
             Pilih Tanggal :
             <input type="hidden" name="menu" value="laporan_dtc">
-            <input type="date" value="<?=isset($_GET['tgl'])?$_GET['tgl'] : date("Y-m-d")?>" name="tanggal" id=""
+            <input type="date" value="<?= isset($_GET['tgl']) ? $_GET['tgl'] : date("Y-m-d") ?>" name="tanggal" id=""
                 class='form-control'> <br>
             <input type="submit" value="PILIH" name='filter' class='btn btn-danger'>
         </form>
     </div>
     <div class="col-md-12">
 
-        <?php 
-    if(isset($_GET['filter'])){
-        $tgl = $_GET['tanggal'];
+        <?php
+        if (isset($_GET['filter'])) {
+            $tgl = $_GET['tanggal'];
         ?>
 
         <div class="table-responsive">
 
-            <table class="table">
+            <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th>NO</th>
@@ -33,37 +33,40 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php 
+                    <?php
 
-                    $cab  = getCabang($id_cabang);
-                    $nama_cabang = strtoupper( $cab['nama_cabang']);
-                    $q_cek  = mysqli_query($con,"SELECT * from temp_laporan_dtc where cabang='$nama_cabang' and tanggal='$tgl'");
-                    if(mysqli_num_rows($q_cek)){
-                        while($r=mysqli_fetch_assoc($q_cek)){
-                            ?>
+                        $cab  = getCabang($id_cabang);
+                        $nama_cabang = strtoupper($cab['nama_cabang']);
+                        $q_cek  = mysqli_query($con, "SELECT * from temp_laporan_dtc where cabang='$nama_cabang' and tanggal='$tgl'");
+                        if (mysqli_num_rows($q_cek)) {
+                            while ($r = mysqli_fetch_assoc($q_cek)) {
+                        ?>
                     <tr>
-                        <td><?=$no++?></td>
-                        <td><?=$r['cabang']?></td>
-                        <td><?=$r['nik']?></td>
-                        <td><?=$r['nama_staff']?></td>
-                        <td><?=$r['tanggal']?></td>
+                        <td><?= $no++ ?></td>
+                        <td><?= $r['cabang'] ?></td>
+                        <td><?= $r['nik'] ?></td>
+                        <td><?= $r['nama_staff'] ?></td>
+                        <td><?= $r['tanggal'] ?></td>
                         <td></td>
-                        <td><?=$r['singkron_laporan']?></td>
+                        <td><?= $r['singkron_laporan'] ?></td>
                         <td>#</td>
                     </tr>
                     <?php
+                            }
+                        } else {
+                            ?>
+                    <tr>
+                        <td colspan="8" align="center">Tidak ada Data!</td>
+                    </tr>
+                    <?php
                         }
-                    }
-                    else{
-                        echo "Tidak ada data";
-                    }
-                    ?>
+                        ?>
                 </tbody>
 
             </table>
         </div>
         <?php
-    }
-    ?>
+        }
+        ?>
     </div>
 </div>
