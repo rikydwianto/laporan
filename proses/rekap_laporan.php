dec3406ee97eac58bc8e58c9df35c3a9e192e7f1
rikydwianto <rikydwianto016@gmail.com>
2024-09-19 17:08:12 +0700

    asass

--- proses/rekap_laporan.php
+++ proses/rekap_laporan.php
@@ -8,44 +8,44 @@ $hari = strtolower(format_hari_tanggal($qtgl));
 $hari = explode(',', $hari)[0];
 ?>
 <div class="col-md-12">
-    <div class="panel-body post-body table-responsive ">
-        <h3 class="page-header">
-            REKAP LAPORAN HARIAN
-        </h3>
-        <form method='get' action='<?php echo $url . $menu ?>rekap_laporan'>
-            <input type=hidden name='menu' value='rekap_laporan' />
-            <input type=date name='tgl' value='<?php echo isset($_GET['tgl']) ? $_GET['tgl'] : date("Y-m-d") ?>'
-                onchange="submit()" />
-            <input type=submit name='cari' value='CARI' />
-        </form>
-        <a href="<?= $url ?>/export/laporan_harian.php?tgl=<?= $qtgl ?>" class='btn btn-success'>
-            <i class="fa fa-file-excel-o"></i> Export To Excel
-        </a>
-        <table class='table table-bordered'>
-            <tr>
-                <th style="vertical-align: middle;" rowspan=3>NO</th>
-                <th style="vertical-align: middle;" rowspan=3>NAMA</th>
-                <th style="vertical-align: middle;" colspan=11 style="text-align:center">LAPORAN
-                    <?php echo format_hari_tanggal($qtgl); ?></th>
-            </tr>
-            <tr>
+	<div class="panel-body post-body table-responsive ">
+		<h3 class="page-header">
+			REKAP LAPORAN HARIAN
+		</h3>
+		<form method='get' action='<?php echo $url . $menu ?>rekap_laporan'>
+			<input type=hidden name='menu' value='rekap_laporan' />
+			<input type=date name='tgl' value='<?php echo isset($_GET['tgl']) ? $_GET['tgl'] : date("Y-m-d") ?>'
+				onchange="submit()" />
+			<input type=submit name='cari' value='CARI' />
+		</form>
+		<a href="<?= $url ?>/export/laporan_harian.php?tgl=<?= $qtgl ?>" class='btn btn-success'>
+			<i class="fa fa-file-excel-o"></i> Export To Excel
+		</a>
+		<table class='table table-bordered'>
+			<tr>
+				<th style="vertical-align: middle;" rowspan=3>NO</th>
+				<th style="vertical-align: middle;" rowspan=3>NAMA</th>
+				<th style="vertical-align: middle;" colspan=11 style="text-align:center">LAPORAN
+					<?php echo format_hari_tanggal($qtgl); ?></th>
+			</tr>
+			<tr>
 
-                <th style="vertical-align: middle;" rowspan="2">CTR</th>
-                <th style="vertical-align: middle;" rowspan="2">AGT</th>
-                <th style="vertical-align: middle;" rowspan="2">CLIENT</th>
-                <th style="vertical-align: middle;" rowspan="2">Bayar</th>
-                <th style="vertical-align: middle;" rowspan="2">Tdk Bayar</th>
-                <th style="vertical-align: middle;" rowspan="2">%</th>
-                <th style="vertical-align: middle;" rowspan="2">Keterangan</th>
-                <th style="vertical-align: middle;" rowspan="2"></th>
-                <th style="vertical-align: middle;text-align:center" colspan="3" style='text-align:center'>RILL </th>
-            </tr>
-            <tr>
-                <th style="vertical-align: middle;">CTR</th>
-                <th style="vertical-align: middle;">AGT</th>
-                <th style="vertical-align: middle;">MEMBER</th>
-            </tr>
-            <?php
+				<th style="vertical-align: middle;" rowspan="2">CTR</th>
+				<th style="vertical-align: middle;" rowspan="2">AGT</th>
+				<th style="vertical-align: middle;" rowspan="2">CLIENT</th>
+				<th style="vertical-align: middle;" rowspan="2">Bayar</th>
+				<th style="vertical-align: middle;" rowspan="2">Tdk Bayar</th>
+				<th style="vertical-align: middle;" rowspan="2">%</th>
+				<th style="vertical-align: middle;" rowspan="2">Keterangan</th>
+				<th style="vertical-align: middle;" rowspan="2"></th>
+				<th style="vertical-align: middle;text-align:center" colspan="3" style='text-align:center'>RILL </th>
+			</tr>
+			<tr>
+				<th style="vertical-align: middle;">CTR</th>
+				<th style="vertical-align: middle;">AGT</th>
+				<th style="vertical-align: middle;">MEMBER</th>
+			</tr>
+			<?php
 			$x = "<i style='color:red;font-weight:bold'>X</i>";
 			$ok = "<b style='color:green;font-weight:bold'>OK</b>";
 			$cek_ka = mysqli_query($con, "SELECT * FROM karyawan,jabatan,cabang where karyawan.id_jabatan=jabatan.id_jabatan and karyawan.id_cabang=cabang.id_cabang and karyawan.id_cabang='$cabang' and jabatan.singkatan_jabatan='SL' and karyawan.status_karyawan='aktif' order by karyawan.nama_karyawan asc");
@@ -69,91 +69,90 @@ $hari = explode(',', $hari)[0];
 						$rill = mysqli_fetch_assoc($qril);
 						echo mysqli_error($con);
 			?>
-            <tr>
-                <td><?php echo $no++ ?>.</td>
+						<tr>
+							<td><?php echo $no++ ?>.</td>
 
-                <td><?php echo $tampil['nama_karyawan'] ?></td>
-                <td><?php echo $tampil_lapor['hitung_center'] ?></td>
-                <td><?php echo $tampil_lapor['member'] ?></td>
-                <td><?php echo $tampil_lapor['anggota'] ?></td>
-                <td><?php echo $tampil_lapor['bayar'] ?></td>
-                <td><?php echo $tidak_bayar = $tampil_lapor['anggota'] - $tampil_lapor['bayar'] ?></td>
-                <td><?php echo round(($tampil_lapor['bayar'] / $tampil_lapor['anggota'] * 100)) ?>%</td>
+							<td><?php echo $tampil['nama_karyawan'] ?></td>
+							<td><?php echo $tampil_lapor['hitung_center'] ?></td>
+							<td><?php echo $tampil_lapor['member'] ?></td>
+							<td><?php echo $tampil_lapor['anggota'] ?></td>
+							<td><?php echo $tampil_lapor['bayar'] ?></td>
+							<td><?php echo $tidak_bayar = $tampil_lapor['anggota'] - $tampil_lapor['bayar'] ?></td>
+							<td><?php echo round(($tampil_lapor['bayar'] / $tampil_lapor['anggota'] * 100)) ?>%</td>
 
-                <td><?php echo $tampil_lapor['keterangan_laporan'] ?></td>
-                <td>
-                    <small><?php echo $tampil_lapor['status_laporan'] ?></small>
-                </td>
-                <td><?= $rill['total_center'] ?>|<?= ($rill['total_center'] === $tampil_lapor['hitung_center'] ? "$ok" : "$x") ?>
-                </td>
-                <td><?= $rill['total_anggota'] ?>|<?= ($rill['total_anggota'] === $tampil_lapor['member'] ? "$ok" : "$x") ?>
-                </td>
-                <td><?= $rill['total_member'] ?>|<?= ($rill['total_member'] === $tampil_lapor['anggota'] ? "$ok" : "$x") ?>
-                </td>
+							<td><?php echo $tampil_lapor['keterangan_laporan'] ?></td>
+							<td>
+								<small><?php echo $tampil_lapor['status_laporan'] ?></small>
+							</td>
+							<td><?= $rill['total_center'] ?>|<?= ($rill['total_center'] === $tampil_lapor['hitung_center'] ? "$ok" : "$x") ?>
+							</td>
+							<td><?= $rill['total_anggota'] ?>|<?= ($rill['total_anggota'] === $tampil_lapor['member'] ? "$ok" : "$x") ?>
+							</td>
+							<td><?= $rill['total_member'] ?>|<?= ($rill['total_member'] === $tampil_lapor['anggota'] ? "$ok" : "$x") ?>
+							</td>
 
-            </tr>
-            <?php
-						$hitung_tdk_bayar = $hitung_tdk_bayar + $tidak_bayar;
+						</tr>
+						<?php
 					} else {
 						if (mysqli_num_rows($cek_l1)) {
 							$tampil_lapor1 = mysqli_fetch_assoc($cek_l1);
 
 						?>
-            <tr>
+							<tr>
 
-                <td><?php echo $no++ ?>.</td>
+								<td><?php echo $no++ ?>.</td>
 
-                <td><?php echo $tampil['nama_karyawan'] ?></td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
+								<td><?php echo $tampil['nama_karyawan'] ?></td>
+								<td>0</td>
+								<td>0</td>
+								<td>0</td>
+								<td>0</td>
+								<td>0</td>
+								<td>0</td>
+								<td>0</td>
+								<td>0</td>
 
-                <td><?php echo $tampil_lapor1['keterangan_laporan'] ?></td>
-                <td><small><i><?php echo $tampil_lapor1['status_laporan'] ?></i></small></td>
+								<td><?php echo $tampil_lapor1['keterangan_laporan'] ?></td>
+								<td><small><i><?php echo $tampil_lapor1['status_laporan'] ?></i></small></td>
 
-            </tr>
-            <?php
+							</tr>
+						<?php
 						} else {
 
 						?>
-            <tr>
-                <td><?php echo $no++ ?>.</td>
+							<tr>
+								<td><?php echo $no++ ?>.</td>
 
-                <td><?php echo $tampil['nama_karyawan'] ?></td>
-                <td colspan=11><i>belum membuat laporan</td>
+								<td><?php echo $tampil['nama_karyawan'] ?></td>
+								<td colspan=11><i>belum membuat laporan</td>
 
-            </tr>
-            <?php
+							</tr>
+					<?php
 						}
 					}
 				} else {
 					?>
-            <tr>
-                <td colspan=6>Belum bikin laporan </td>
-            </tr>
-            <?php
+					<tr>
+						<td colspan=6>Belum bikin laporan </td>
+					</tr>
+				<?php
 				}
 				?>
 
-            <?php
+			<?php
 
 			}
 			?>
-            <tr>
-                <th colspan=2 class='text-center'>Total</th>
-                <th><?php echo $hitung_center ?></th>
-                <th><?php echo $hitung_member ?></th>
-                <th><?php echo $hitung_agt ?></th>
-                <th><?php echo $hitung_bayar ?></th>
-                <th><?php echo $hitung_tdk_bayar ?></th>
-                <th colspan=9><?php echo $persen = round(($hitung_bayar / $hitung_agt) * 100, 2) ?>%</th>
-            </tr>
-        </table>
+			<tr>
+				<th colspan=2 class='text-center'>Total</th>
+				<th><?php echo $hitung_center ?></th>
+				<th><?php echo $hitung_member ?></th>
+				<th><?php echo $hitung_agt ?></th>
+				<th><?php echo $hitung_bayar ?></th>
+				<th><?php echo $hitung_tdk_bayar ?></th>
+				<th colspan=9><?php echo $persen = round(($hitung_bayar / $hitung_agt) * 100, 2) ?>%</th>
+			</tr>
+		</table>
 
-    </div>
+	</div>
 </div>
